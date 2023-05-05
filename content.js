17ab4b74be01709edcd0cc3e586d0b9ea2aa25c5
160294w <160294w@gmail.com>
2023-05-05 20:51:48 +0900

    Add: attirbute notranslate class for code tags

--- content.js
+++ content.js
@@ -1,6 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var codeTags = document.getElementsByTagName('code');
-  for (var i = 0; i < codeTags.length; i++) {
-    codeTags[i].setAttribute('translate', 'no');
-  }
-});
+window.addEventListener("load", main, false);
+
+function main(e) {
+    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+    function jsLoaded() {
+	if (document.getElementsByTagName('code') != null) {
+	    codeTags = document.getElementsByTagName('code') 
+	    clearInterval(jsInitCheckTimer);
+	    for (var i = 0; i < codeTags.length; i++) {
+		codeTags[i].setAttribute('translate', 'no');
+	    }
+	    for (var i = 0; i < codeTags.length; i++) {
+		codeTags[i].setAttribute('class', 'notranslate');
+	    }
+	}
+    }
+};
